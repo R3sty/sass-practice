3c14667d2fdec470c83c4c6e9e94434b95df2c27
R3sty <iral.resty@gmail.com>
2023-03-22 23:49:21 +0900

    set up globals for scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import styles from './App.module.scss';
 
 function App() {
-	return <div className={styles.App}>App works</div>;
+	return (
+		<div className={styles.App}>
+			<h1>Mythos</h1>
+			<p>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores non
+				quidem asperiores sed maiores natus, rem molestias suscipit id quibusdam
+				dolorum ullam totam quia quo doloribus, hic veritatis minus veniam? Odit
+				minima autem voluptates atque architecto adipisci ipsum ab quo,
+				asperiores praesentium quia quam, nesciunt amet velit perspiciatis illo
+				omnis dolorum placeat eius exercitationem molestias sequi incidunt
+				ratione?
+			</p>
+		</div>
+	);
 }
 
 export default App;

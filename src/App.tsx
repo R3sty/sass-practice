dcb48b9cd81ca8144ee4a4d807413890458bd089
R3sty <iral.resty@gmail.com>
2023-03-23 11:26:16 +0900

    added layout dir

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,28 @@ import styles from './App.module.scss';
 
 function App() {
 	return (
-		<div className={styles.App}>
-			<h1>Mythos</h1>
-			<p>
-				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores non
-				quidem asperiores sed maiores natus, rem molestias suscipit id quibusdam
-				dolorum ullam totam quia quo doloribus, hic veritatis minus veniam? Odit
-				minima autem voluptates atque architecto adipisci ipsum ab quo,
-				asperiores praesentium quia quam, nesciunt amet velit perspiciatis illo
-				omnis dolorum placeat eius exercitationem molestias sequi incidunt
-				ratione?
-			</p>
+		<div className="grid">
+			<main className="grid__main">
+				<h1>Mythos</h1>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores non
+					quidem asperiores sed maiores natus, rem molestias suscipit id
+					quibusdam dolorum ullam totam quia quo doloribus, hic veritatis minus
+					veniam? Odit minima autem voluptates atque architecto adipisci ipsum
+					ab quo, asperiores praesentium quia quam, nesciunt amet velit
+					perspiciatis illo omnis dolorum placeat eius exercitationem molestias
+					sequi incidunt ratione?
+				</p>
+			</main>
+			<aside className="grid__sidebar">
+				<h2>More Info</h2>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+					in, repellat dolore recusandae ratione modi omnis provident sunt autem
+					numquam, nisi adipisci at accusantium odio facere explicabo. Commodi,
+					error at.
+				</p>
+			</aside>
 		</div>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './scss/globals/_index.scss';
+import './scss/styles.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
